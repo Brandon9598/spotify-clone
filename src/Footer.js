b5ce1f9a45a49ff562f02acd4f7878781c3a749c
brandon9598 <brandon9598@gmail.com>
2020-12-23 02:35:11 -0500

    wrap up app

--- src/Footer.js
+++ src/Footer.js
@@ -13,7 +13,11 @@ function Footer() {
   return (
     <div className="footer">
       <div className="footer__left">
-        <img className="footer_albumLogo" src="" alt="" />
+        <img
+          className="footer_albumLogo"
+          src="https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Usher_-_Confessions_album_cover.jpg/220px-Usher_-_Confessions_album_cover.jpg"
+          alt=""
+        />
         <div className="footer__songInfo">
           <h4>Yeah!</h4>
           <p>Usher</p>
