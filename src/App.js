44477d548971a419dca2b6dca9187dcbb837e898
brandon9598 <brandon9598@gmail.com>
2020-12-23 01:11:03 -0500

    pulling playlist from spotify now

--- src/App.js
+++ src/App.js
@@ -32,6 +32,13 @@ function App() {
           });
         })
         .catch((err) => console.log(err));
+
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists: playlists,
+        });
+      });
     }
   }, []);
 
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Sidebar.css";
 import SidebarOption from "./SidebarOption";
-
 import HomeIcon from "@material-ui/icons/Home";
 import SearchIcon from "@material-ui/icons/Search";
 import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
+import { useDataLayerValue } from "./DataLayer";
 
 function Sidebar() {
+  const [{ playlists }, dispatch] = useDataLayerValue();
+
   return (
     <div className="sidebar">
       <img
@@ -24,6 +26,10 @@ function Sidebar() {
       <br />
       <strong className="sidebar__title">PLAYLISTS</strong>
       <hr />
+
+      {playlists?.items?.map((playlist) => (
+        <SidebarOption title={playlist.name} />
+      ))}
 
       <SidebarOption title="Hip Hop"></SidebarOption>
       <SidebarOption title="Rock"></SidebarOption>
