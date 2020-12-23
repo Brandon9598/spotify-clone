1ed281db3db438f758c7822a27d92a79cce5ec74
brandon9598 <brandon9598@gmail.com>
2020-12-23 01:04:22 -0500

    side bar working with hardcoded values

--- src/Player.js
+++ src/Player.js
@@ -1,10 +1,18 @@
 import React from "react";
+import Body from "./Body";
 import "./Player.css";
+import Sidebar from "./Sidebar";
+import Footer from "./Footer";
 
 function Player({ spotify }) {
   return (
     <div className="player">
-      <div className="player__body"></div>
+      <div className="player__body">
+        <Sidebar />
+        <Body />
+      </div>
+
+      <Footer />
     </div>
   );
 }
